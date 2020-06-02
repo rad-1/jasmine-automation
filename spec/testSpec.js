526b5a15d8b02b5af09779d68e96153150bb1307
rad-1 <jctipton123@gmail.com>
2020-06-02 17:22:41 -0400

    Add test file

---
+++ spec/testSpec.js
@@ -0,0 +1,18 @@
+const webdriver = require('selenium-webdriver')
+const driver = new webdriver.Builder()
+    .forBrowser(`chrome`)
+    .build();
+
+it('User can log in', async () => {
+  await driver.get('http://automationpractice.com/index.php');
+  await driver.findElement(By.xpath('//div[@class="header_user_info"]')).click()
+  await driver.wait(until.elementLocated(By.id('email')));
+  await driver.findElement(By.id('email')).sendKeys('example@netguru.com')
+  await driver.findElement(By.id('passwd')).sendKeys('password', Key.RETURN)
+  await driver.wait(until.titleContains("My account"));
+  driver
+    .getTitle()
+    .then(title => {
+      expect(title).toEqual('My account - My Store');
+    });
+});
