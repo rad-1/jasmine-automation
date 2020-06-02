77e7b6b3460ea9391db1a493f08ebf41bbe16558
rad-1 <jctipton123@gmail.com>
2020-06-02 17:30:31 -0400

    Fix test

--- spec/testSpec.js
+++ spec/testSpec.js
@@ -1,12 +1,12 @@
 // Test article: https://www.netguru.com/codestories/which-javascript-ui-testing-framework-to-use-in-2020
 // Test website: http://automationpractice.com/index.php
 
-const webdriver = require('selenium-webdriver')
-const driver = new webdriver.Builder()
-    .forBrowser(`chrome`)
-    .build();
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
 it('User can log in', async () => {
+  let driver = await new Builder().forBrowser('chrome').build();
   await driver.get('http://automationpractice.com/index.php');
   await driver.findElement(By.xpath('//div[@class="header_user_info"]')).click()
   await driver.wait(until.elementLocated(By.id('email')));
@@ -18,4 +18,5 @@ it('User can log in', async () => {
     .then(title => {
       expect(title).toEqual('My account - My Store');
     });
+  await driver.quit();
 });
